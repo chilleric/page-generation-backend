c642b67a344886357f6e8f0526078e2f4a02be64
ericchentch <ericchen.tch@gmail.com>
2022-12-04 14:37:48 +0700

    init

---
+++ src/main/java/com/chillleric/page_generation/PageGenerationApplication.java
@@ -0,0 +1,13 @@
+package com.chillleric.page_generation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageGenerationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PageGenerationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chillleric/page_generation/PageGenerationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chillleric.page_generation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PageGenerationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
